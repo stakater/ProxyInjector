d8098f1086ee37c240b45f77916ec5fec93d1709
Ahmad Iqbal <ahmad@aurorasolutions.io>
2018-10-12 20:03:24 +0500

    fix patch

--- internal/pkg/callbacks/resourceActionFuncs.go
+++ internal/pkg/callbacks/resourceActionFuncs.go
@@ -9,6 +9,14 @@ func GetDeploymentAnnotations(resource interface{}) map[string]string {
 	return resource.(*v1beta1.Deployment).GetAnnotations()
 }
 
+func GetDeploymentName(resource interface{}) string {
+	return resource.(*v1beta1.Deployment).Name
+}
+
+func GetDeploymentNamespace(resource interface{}) string {
+	return resource.(*v1beta1.Deployment).Namespace
+}
+
 // GetDeploymentContainers returns the containers of given deployment
 func GetDeploymentContainers(item interface{}) []v1.Container {
 	return item.(v1beta1.Deployment).Spec.Template.Spec.Containers
--- internal/pkg/handler/create.go
+++ internal/pkg/handler/create.go
@@ -4,82 +4,86 @@ import (
 	logger "github.com/sirupsen/logrus"
 	"github.com/stakater/ProxyInjector/internal/pkg/callbacks"
 	"github.com/stakater/ProxyInjector/internal/pkg/constants"
+	"github.com/stakater/ProxyInjector/pkg/kube"
 	//"k8s.io/client-go/util/retry"
 	"encoding/json"
-	"k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
 // ResourceCreatedHandler contains new objects
 type ResourceCreatedHandler struct {
-	//client *kubernetes.Clientset,
 	Resource interface{}
 }
 
-type patchValue struct {
-	Op    string `json:"op"`
-	Path  string `json:"path"`
-	Value struct {
-		Name         string `json:"name"`
-		Image        string `json:"image"`
-		VolumeMounts struct {
-			Name      string `json:"name"`
-			MountPath string `json:"mountPath"`
-		}
-	}
+type ContainerVolumes struct {
+	Name      string `json:"name"`
+	MountPath string `json:"mountPath"`
 }
 
-/*Name:  "web",
-Image: "nginx:1.12",
-Ports: []v1.ContainerPort{{
-Name:          "http",
-Protocol:      v1.ProtocolTCP,
-ContainerPort: 80,
-}},*/
+type Container struct {
+	Name         string           `json:"name"`
+	Image        string           `json:"image"`
+	VolumeMounts ContainerVolumes `json: "volumeMounts"`
+}
+
+type patch struct {
+	Op    string    `json:"op"`
+	Path  string    `json:"path"`
+	Value Container `json: "value"`
+}
+
 // Handle processes the newly created resource
 func (r ResourceCreatedHandler) Handle() error {
 	if r.Resource == nil {
 		logger.Errorf("Resource creation handler received nil resource")
 	} else {
 		logger.Info("Resource created")
+		name := callbacks.GetDeploymentName(r.Resource)
+		namespace := callbacks.GetDeploymentNamespace(r.Resource)
 		annotations := callbacks.GetDeploymentAnnotations(r.Resource)
 		value := annotations[constants.AuthProxyUpstreamAnnotation]
+
 		logger.Info(value)
 
-		logger.Info("Updating deployment...")
+		if value != "" {
 
-		payload := []patchValue{{
-			Op:   "merge",
-			Path: "/spec/template/spec/containers",
-			Value: {
-				Name:  "proxy",
-				Image: "quay.io/gambol99/keycloak-proxy:v2.1.1",
-				VolumeMounts: {
-					Name:      "keycloak-proxy-config",
-					MountPath: "/etc/config",
+			logger.Infof("Updating deployment ... %s", name)
+			payload := []patch{{
+				Op:   "add",
+				Path: "/spec/template/spec/containers",
+				Value: Container{
+					Name:  "proxy",
+					Image: "quay.io/gambol99/keycloak-proxy:v2.1.1",
+					VolumeMounts: ContainerVolumes{
+						Name:      "keycloak-proxy-config",
+						MountPath: "/etc/config",
+					},
 				},
-			},
-		}}
+			}}
 
-		payloadBytes, _ := json.Marshal(payload)
-		_, err := r.Resource.(*v1beta1.Deployment).Patch(r.Resource.(*v1beta1.Deployment).name, types.JSONPatchType, payloadBytes)
-		return err
+			client, err := kube.GetClient()
+			if err == nil {
+				payloadBytes, _ := json.Marshal(payload)
+				client.AppsV1beta1().Deployments(namespace).Patch(name, types.StrategicMergePatchType, payloadBytes)
+			} else {
+				logger.Error(err)
+			}
 
-		/*			&v1.Container{
-						Name:  "web",
-						Image: "nginx:1.12",
-						Ports: []v1.ContainerPort{
-							{
-								Name:          "http",
-								Protocol:      v1.ProtocolTCP,
-								ContainerPort: 80,
+			/*			&v1.Container{
+							Name:  "web",
+							Image: "nginx:1.12",
+							Ports: []v1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      v1.ProtocolTCP,
+									ContainerPort: 80,
+								},
 							},
-						},
-					}
-		*/
+						}
+			*/
 
-		logger.Info("Updated deployment...")
+			logger.Info("Updated deployment...")
+		}
 	}
 	return nil
 }
