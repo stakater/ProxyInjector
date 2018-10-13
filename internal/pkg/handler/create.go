61d9c2a7d108f6996e6b92e767a85e2746840d00
Ahmad Iqbal <ahmad@aurorasolutions.io>
2018-10-13 15:22:49 +0500

    cleanup

--- internal/pkg/handler/create.go
+++ internal/pkg/handler/create.go
@@ -26,12 +26,6 @@ type Container struct {
 	VolumeMounts ContainerVolumes `json: "volumeMounts"`
 }
 
-/*type patch struct {
-	Op    string    `json:"op"`
-	Path  string    `json:"path"`
-	Value Container `json: "value"`
-}*/
-
 type Spec2 struct {
 	Containers []Container `json:"containers"`
 }
@@ -64,27 +58,6 @@ func (r ResourceCreatedHandler) Handle() error {
 		if value != "" {
 
 			logger.Infof("Updating deployment ... %s", name)
-			/*payload := patch{
-				Op:   "add",
-				Path: "/spec/template/spec/containers",
-				Value: Container{
-					Name:  "proxy",
-					Image: "quay.io/gambol99/keycloak-proxy:v2.1.1",
-					VolumeMounts: ContainerVolumes{
-						Name:      "keycloak-proxy-config",
-						MountPath: "/etc/config",
-					},
-				},
-			}*/
-
-			/*payload := Container{
-				Name:  "proxy",
-				Image: "quay.io/gambol99/keycloak-proxy:v2.1.1",
-				VolumeMounts: ContainerVolumes{
-					Name:      "keycloak-proxy-config",
-					MountPath: "/etc/config",
-				},
-			}*/
 
 			payload := patch{
 				Spec: Spec1{
@@ -106,7 +79,6 @@ func (r ResourceCreatedHandler) Handle() error {
 			client, err := kube.GetClient()
 			if err == nil {
 				payloadBytes, err3 := json.Marshal(payload)
-				//deployment, err2 := client.ExtensionsV1beta1().Deployments(namespace).Patch(name, types.StrategicMergePatchType, valueBytes,"/spec/template/spec/containers")
 
 				if err3 == nil {
 					deployment, err2 := client.ExtensionsV1beta1().Deployments(namespace).Patch(name, types.StrategicMergePatchType, payloadBytes)
@@ -122,19 +94,6 @@ func (r ResourceCreatedHandler) Handle() error {
 			} else {
 				logger.Error(err)
 			}
-
-			/*			&v1.Container{
-							Name:  "web",
-							Image: "nginx:1.12",
-							Ports: []v1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      v1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						}
-			*/
 
 		}
 	}
