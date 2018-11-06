63307161747b97f5a30ff4ec23a1a11a0a4d2bf3
Ahmad Iqbal <ahmad@aurorasolutions.io>
2018-11-06 18:03:22 +0500

    make relevant global configuration overridable from individual app

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 )
 
 type Config struct {
@@ -44,62 +43,11 @@ func ReadConfig(filePath string) Config {
 	return config
 }
 
-func GetControllerConfig() []string {
+func GetControllerConfig() Config {
 	configFilePath := os.Getenv("CONFIG_FILE_PATH")
 	if len(configFilePath) == 0 {
 		configFilePath = "config.yaml"
 	}
 
-	config := ReadConfig(configFilePath)
-
-	return structToStringArray(config)
-}
-
-func structToStringArray(config Config) []string {
-
-	configArgs := []string{}
-
-	if config.ClientId != "" {
-		configArgs = append(configArgs, "--client-id="+config.ClientId)
-	}
-	if config.ClientSecret != "" {
-		configArgs = append(configArgs, "--client-secret="+config.ClientSecret)
-	}
-	if config.DiscoveryUrl != "" {
-		configArgs = append(configArgs, "--discovery-url="+config.DiscoveryUrl)
-	}
-	/*	if config.EnableDefaultDeny !="" {
-		configArgs = append(configArgs, " --enable-default-deny=\""+config.EnableDefaultDeny+"\"")
-	}*/
-	if config.Listen != "" {
-		configArgs = append(configArgs, "--listen="+config.Listen)
-	}
-	if config.SecureCookie != "" {
-		configArgs = append(configArgs, "--secure-cookie="+config.SecureCookie)
-	}
-	if config.Verbose != "" {
-		configArgs = append(configArgs, "--verbose="+config.Verbose)
-	}
-	if config.EnableLogging != "" {
-		configArgs = append(configArgs, "--enable-logging="+config.EnableLogging)
-	}
-	for _, origin := range config.CorsOrigins {
-		configArgs = append(configArgs, "--cors-origins="+origin)
-	}
-	for _, method := range config.CorsMethods {
-		configArgs = append(configArgs, "--cors-methods="+method)
-	}
-	for _, resource := range config.Resources {
-		//  --resources "uri=/admin*|roles=admin,superuser|methods=POST,DELETE
-		res := ""
-		if resource.URI != "" {
-			res = "uri=" + resource.URI
-		}
-		if len(resource.Methods) != 0 {
-			res = res + "|methods=" + strings.Join(resource.Methods, ",")
-		}
-		configArgs = append(configArgs, "--resources="+res)
-	}
-
-	return configArgs
+	return ReadConfig(configFilePath)
 }
